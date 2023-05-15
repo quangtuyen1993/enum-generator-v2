7fe52d4441183a2b2848cf4a372dac889fccc81e
Nathan Pham <nathan.pham@legato.co>
2023-05-15 07:41:54 +0700

    supports flutter3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.1.0
+* Supports flutter3
+## 1.0.9
+* Fix cannot generate files
 ## 1.0.8
 * Update README.md documents
 ## 1.0.7
--- test/enum_generator_test.dart
+++ test/enum_generator_test.dart
@@ -1,5 +1,1 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:enum_generator/enum_generator.dart';
-
 void main() {}
